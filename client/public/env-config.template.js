755a71f7976ea917d6119e04639e21ebc884707a
Nayandeep Goswami <nayandg8@gmail.com>
2025-03-18 18:59:52 +0530

    Update environment configuration and gitignore

---
+++ client/public/env-config.template.js
@@ -0,0 +1,5 @@
+// This file is a template for env-config.js
+// Copy this file to env-config.js and fill in your API keys
+window.ENV = {
+  NEXT_PUBLIC_HUGGINGFACE_API_KEY: "your_huggingface_api_key_here",
+}; 
